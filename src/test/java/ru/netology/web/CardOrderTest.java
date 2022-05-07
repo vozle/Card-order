4ce7b4d260749df1876e1e0535349ed84f45b06c
vozle <vozle2@gmail.com>
2022-05-08 00:51:42 +0300

    Add validation check test

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -49,4 +49,15 @@ public class CardOrderTest {
         String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    @Test
+    public void shouldTestInvalidNameWebForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Kruzenshtern Ivan");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79103214477");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
 }
